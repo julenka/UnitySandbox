b43005e2273ea4ca6e3e0384c3e78be526df4712
Julia Schwarz <julenka@cs.cmu.edu>
2016-07-02 01:04:40 -0700

    Add Spinny experiment, figure out how to do rotations properly

---
+++ Assets/Spinny/SpinControl.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Provides ability to control the spin of an object via angular velocity.
+/// Spin can be dampened
+/// </summary>
+class SpinControl : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    public float TorqueMagnitude;
+    public float TorqueDampen = 0.7f;
+
+    // Degrees per second in X, Y, Z axes
+    private Vector3 angularVelocity;
+
+    private Quaternion initialRotation;
+
+    public void IncreaseSpin(Vector3 amount)
+    {
+        angularVelocity += amount;
+    }
+
+    public void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        initialRotation = transform.localRotation;
+    }
+
+    public void Update()
+    {
+        // update velocity
+        if (Input.GetKey(KeyCode.Space))
+        {
+            IncreaseSpin(Vector3.up * TorqueMagnitude * Time.deltaTime);
+        }
+        angularVelocity *= (1f - TorqueDampen * Time.deltaTime);
+
+        // Update rotation
+        var rotationDelta = Quaternion.EulerAngles(angularVelocity * Time.deltaTime);
+        transform.localRotation = rotationDelta * transform.localRotation;
+
+        // Check reset condition (TODO: refactor?)
+        if (angularVelocity.magnitude < 0.05f)
+        {
+            transform.localRotation = initialRotation;
+        }
+    }
+}
---
+++ Assets/Spinny/SpinOnCollision.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinOnCollision : MonoBehaviour
+{
+
+    private Vector3 previousPosition;
+    public LayerMask layerMask;
+    public float TorqueMultiplier = 1000;
+    public float Exponent = 2;
+
+    // Use this for initialization
+    void Start()
+    {
+        previousPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var direction = transform.position - previousPosition;
+        var distance = direction.magnitude;
+        Debug.DrawLine(transform.position, previousPosition);
+        var allHit = Physics.RaycastAll(previousPosition, direction, distance, layerMask);
+
+        foreach (var hitInfo in allHit)
+        {
+            var torqueTest = hitInfo.collider.gameObject.GetComponent<SpinControl>();
+            if (torqueTest != null)
+            {
+                torqueTest.IncreaseSpin(Vector3.up * -1f * Mathf.Sign(Vector3.Dot(direction, transform.right)) * Mathf.Pow(distance, Exponent) * TorqueMultiplier);
+                Debug.DrawRay(hitInfo.point, Vector3.up, Color.red);
+            }
+        }
+
+        previousPosition = transform.position;
+    }
+}
